5be6b22e5bfbed9231102a3eb8bd1e7766f8c6f2
cosmin_louis <32954953+cosmineala@users.noreply.github.com>
2018-11-30 17:09:38 +0200

    0.8

--- p10.py
+++ p10.py
@@ -1,24 +1,28 @@
+from random import *
+from math import *
+
+# Determinarea numarului de elemente ( 3  4 )
 def nr_elemente(lis):
 
 	return lis.count()
 
 
-
+# Afisarea multimii ( 2 )
 def multime_print(lis):
 
 	print(lis)
 
 
-
+# Determinarea suportului ( 6 )
 def suport(lis):
 
 	sup = lis.copy()
 
 	sup.sort()
 
-	n = 0
+	n = lis.count()
 
-	while sup[n] != sup[-1]:
+	while n:
 
 		m = sup.count( sup[n] ) - 1
 
@@ -28,40 +32,96 @@ def suport(lis):
 
 			m = m - 1
 
-	n = n + 1
+		n = n - 1
 
 	return sup
 
 
 
 
-
+# Crearea multimii ( 1 )
 def gen( lis ):
 
-	n = random.randint( 1 , 101 )
+	n = randint( 1 , 101 )
 
 	while n:
 		
-		lis.insert( random.randint( 1 , 101 ) )
+		lis.append( randint( 1 , 101 ) )
 
 		n = n - 1
 
-
-
+# Adaugare de termeni ( 11 )
 def inserare( lis ):
 
 	n = int( input("Numar de termeni >>> ") )
 
 	while n:
 		
-		lis.insert( int( input( ">>> " ) ) )
+		lis.append( int( input( ">>> " ) ) )
+
+
+# Verifica daca lista este inclusa in alta lista ( 8 )
+def if_inlude(lis):
+
+	lis2 = []
+	gen(lis2)
+
+	sup_lis = []
+	sup_lis2 = []
+
+	sup_lis = suport(lis)
+	sup_lis2 = suport(lis2)
+
+	for i in range( 0 , sup_lis.count() - 1) :
+
+		if sup_lis[i] in sup_lis2 :
+			return "Este inclusa"
+
+	return "Nu este inclusa"
+
+# Operatiile de reuniune diferenta si incluziune
+def operation(lis):
+
+	 lis2 = []
+	 gen(lis2)
+
+	 print( "Reuniune : " + str( lis | lis2) )
+	 print( "Intersectie : " + str( lis & lis2 ) )
+	 print( "Diferenta : " + str( lis - lis2 ) )
+
+
+def if_inlude_who(lis):
+
+	lis2 = []
+	gen(lis2)
+
+	sup_lis = []
+	sup_lis2 = []
+
+	sup_lis = suport(lis)
+	sup_lis2 = suport(lis2)
+
+	if sup_lis.count() < sup_lis2.count() :
+
+		for i in range( 0 , sup_lis.count() - 1) :
+
+			if sup_lis[i] in sup_lis2 :
+				return "Arima multime nu este inclusa in cea de a doua"
+
+		return "Arima multime Este inclusa in cea de a doua"
+
+	else :
+
+		for i in range( 0 , sup_lis2.count() - 1) :
+
+			if sup_lis2[i] in sup_lis1 :
+				return "A doua multime nu este inclusa in prima"
+
+		return "A doua multime Este inclusa in prima"
 
 
 
-
-
-
-
+# Meniul
 def meniu(lis):
 
 	print("Exit >>> 0")
@@ -72,6 +132,9 @@ def meniu(lis):
 	print("Determinarea daca un element apartine multimii >>> 5")
 	print("Determinarea suportul multimii multiple >>> 6")
 	print("Determinarea numarului k(e, M), pentru un element e specificat >>> 7")
+	print("Determinarea daca multimea multipla curenta este inclusa in alta multime specificata >>> 8")
+	print("Operatiile de reuniune, diferenta si intersectie a doua multimi multiple >>> 9")
+	print("Compararea a doua multimi pe baza operatiei de incluziune >>> 10")
 
 	m = int( input( ">>> " ) )
 
@@ -85,15 +148,39 @@ def meniu(lis):
 		multime_print(lis)
 
 	if m == 3 :
-		nr_elemente(lis)
+		print( nr_elemente(lis) )
 
 	if m == 4 :
 		if nr_elemente(lis) :
 			print("Multimea nu este vida")
-		else
+		else :
 			print("Multimea este vida")
 
 	if m == 5 :
+		if int( input(">>> ") ) in lis :
+			print("Termenul este in lista")
+		else :
+			print("Termenul NU este in lista")
+
+	if m == 6 :
+		print( suport( lis ) )
+
+	if m == 7 :
+		print( lis.count( int( input("Introduceti elementul e \n>>>") ) ) )
+
+	if m == 8 :
+		print( if_include(lis) )
+
+	if m == 9 :
+		operation(lis)
+
+	if m == 10 :
+		print( if_inlude_who )
+
+	if m == 11 :
+		inserare( lis )
+
+	meniu(lis)
 
 
 
@@ -101,3 +188,6 @@ def meniu(lis):
 
 lis = []
 
+meniu(lis)
+
+print("Program terminat")
